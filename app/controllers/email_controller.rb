43b196cde3e06ccb8d9af44f5d757ded513ec4c5
Alan Cohen <awc583@gmail.com>
2014-09-09 11:13:55 -0400

    fixed the alchemy api in email server

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -4,22 +4,33 @@ class EmailController < ApplicationController
 
   def create
     sender = EmailParser.sender(params)
+    email_body = EmailParser.content(params)
 
-    content = "Hello and thank you for using Should I Send This?  Below you will find the content of your email."
-    content << "\n\n\n"
-    content << EmailParser.content(params)
+    response = "Hello and thank you for using Should I Send This?"
+    response << "\n\n"
 
-    # TODO - alchemy api not working properly
+    alchemist = AlchemyData.new(email_body)
 
-    # alchemist = AlchemyData.new(content)
+    if alchemist
+      sentiment = (alchemist.sentiment*100).floor
 
-    # if alchemist
-    #   sentiment = alchemist.sentiment
-    # else
-    #   sentiment = "Should I Send This? ...is down for maintenance. Try again later!"
-    # end
+      if sentiment > 0
+        response << "Your email is #{sentiment}% positive.  If you are happy with this, please send!"
+      else
+        response << "Your email is #{sentiment.abs}% negative.  Please consider a revision!"
+      end
+    else
+      response << "Should I Send This? ...is down for maintenance. Try again later!"
+    end
 
-    UserMailer.email(sender, content).deliver
+    response << "\n\n\n\n"
+    response << "Below is the content of your email:"
+    response << "\n\n"
+    response << email_body
+
+    UserMailer.email(sender, response).deliver
+
+    redirect_to :root
   end
 end
 
