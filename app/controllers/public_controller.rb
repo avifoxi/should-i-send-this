2899721cb63076acf6f54766eb2e33303a1cef7c
Avi Fox-Rosen <frosenox@gmail.com>
2014-09-09 10:01:29 -0400

    splash page renders once bc of cookie assignment in public controller

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -2,6 +2,12 @@ class PublicController < ApplicationController
   layout "index"
 
   def index
+    unless cookies[:seen_splash] == 'yes'
+      @show_splash = true   #serve up one thing with jquery fade out
+      cookies[:seen_splash] = 'yes'
+    else
+
+    end
 
   end
 
