cece5bc83c5c4e1f1eccdc6b59027471416a7b0c
Alan Cohen <awc583@gmail.com>
2014-09-09 10:50:10 -0400

    wrote tests for documents controller

--- spec/controllers/documents_controller_spec.rb
+++ spec/controllers/documents_controller_spec.rb
@@ -4,7 +4,60 @@ RSpec.describe DocumentsController, :type => :controller do
   context "#index" do
     it "assigns documents to a variable" do
       get :index
-      expect(assigns(:documents)).not_to be_nil
+      expect(:documents).not_to be_nil
+    end
+  end
+
+  context "#create" do
+    it "assigns a document to a variable" do
+      post :create
+      expect(:document).not_to be_nil
+    end
+
+    it "assigns a version to a variable" do
+      post :create
+      expect(:version).not_to be_nil
+    end
+  end
+
+  context "#create" do
+    it "assigns a document to a variable" do
+      get :new
+      expect(:document).not_to be_nil
+    end
+
+    it "assigns a version to a variable" do
+      get :new
+      expect(:version).not_to be_nil
+    end
+  end
+
+  context "#show" do
+    it "assigns a document to a variable" do
+      get :show, id: "1"
+      expect(:document).not_to be_nil
+    end
+
+    it "assigns a version to a variable" do
+      get :show, id: "1"
+      expect(:version).not_to be_nil
+    end
+  end
+
+  context "#update" do
+    it "assigns a document to a variable" do
+      expect(:document).not_to be_nil
+    end
+  end
+
+  context "#destroy" do
+    it "assigns a document to a variable" do
+      expect(:document).not_to be_nil
     end
   end
 end
+
+
+
+
+
