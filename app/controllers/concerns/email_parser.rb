b064848c96da20c9afce6b8f70df15b599d2836e
AviFoxi <frosenox@gmail.com>
2014-09-10 16:46:38 -0400

    refactor email parser class, slim down email controller

--- app/controllers/concerns/email_parser.rb
+++ app/controllers/concerns/email_parser.rb
@@ -1,14 +1,42 @@
-module EmailParser
+class EmailParser 
+	
+	def initialize(params)
+		@sender = params['sender']
+		@subject = params['subject']
+		@content = params['stripped-text']
+		@user = nil
+		@document = nil
+		@version = nil
+	end
 
-  def self.sender(params)
-   params['sender']
+  def create_document_version_for_user  	
+  	return unless find_user
+ 
+  	save_document
+
+  	if @document
+  		save_version
+  	end
+  	return true
   end
 
-  def self.subject(params)
-    params['subject']
+  private
+
+  def save_document
+		document = Document.new(title: @subject, context: @subject, privacy: true)
+		if document.save
+			@document = document
+		end
   end
 
-  def self.content(params)
-    params['stripped-text']
+  def save_version
+  	@version = Version.new(content: @content)
+  	@document.versions << @version
+  	@user.documents.last.versions << @version
   end
+
+  def find_user
+  	@user = User.find_by_email(@sender)
+  end
+
 end
