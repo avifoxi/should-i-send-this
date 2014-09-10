6c543c6cee4016b172b3be7fde9f84a11878406a
Alan Cohen <awc583@gmail.com>
2014-09-10 18:04:38 -0400

    updated seed file

--- app/controllers/versions_controller.rb
+++ app/controllers/versions_controller.rb
@@ -25,9 +25,9 @@ class VersionsController < ApplicationController
     @comments = @version.comments.display_order
     @comment = @version.comments.build
 
-    # @alchemist = AlchemyData.new(@version.content)
+    @alchemist = AlchemyData.new(@version.content)
     # UNCOMMENT BELOW FOR NON-API CALL DEVELOPER MODE
-    @alchemist = ErrorAlchemist.new
+    # @alchemist = ErrorAlchemist.new
 
     unless @alchemist
       @alchemist = ErrorAlchemist.new
