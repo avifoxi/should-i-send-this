7253849c76b8f57062146c3a8483d5703bd8dbb8
Alan Cohen <awc583@gmail.com>
2014-09-11 10:31:37 -0400

    added protections so that content with less than three keywords will not crash system

--- app/helpers/versions_helper.rb
+++ app/helpers/versions_helper.rb
@@ -1,38 +1,44 @@
 module VersionsHelper
- def add_highlight_spans(version, alchemist)
-   content = version.content
-   keywords = three_keywords_with_variations(alchemist.keywords)
+  def add_highlight_spans(version, alchemist)
+    content = version.content
+    keywords = three_keywords_with_variations(alchemist.keywords)
 
-   keywords.each do |k, v|
-     wrap_word_in_span(k, v, content)
-   end
+    unless keywords['high'] == nil
+      keywords.each do |k, v|
+        wrap_word_in_span(k, v, content)
+      end
+    end
 
-   content.html_safe
- end
+    content.html_safe
+  end
 
- private
+  private
 
- def three_keywords_with_variations(keywords)
-   top_three = keywords.take(3)
-   ratings_hash = {high: nil, medium: nil, low: nil}
+  def three_keywords_with_variations(keywords)
+    top_three = keywords.take(3)
+    ratings_hash = {high: nil, medium: nil, low: nil}
 
-   i = 0
-   ratings_hash.each_key do |key|
-     word = top_three[i]
-     gamut = [word, word.pluralize, word.singularize, word.capitalize, word.downcase].uniq
-     ratings_hash[key] = gamut
-     i += 1
-   end
-   ratings_hash
- end
+    unless keywords.length < 3
+      i = 0
+      ratings_hash.each_key do |key|
+        word = top_three[i]
+        gamut = [word, word.pluralize, word.singularize, word.capitalize, word.downcase].uniq
+        ratings_hash[key] = gamut
+        i += 1
+      end
+    end
 
- def wrap_word_in_span(a, b, content)
-   content = content
+    ratings_hash
+  end
 
-   b.each do |word|
-     expr = /\b#{word}\b/
-     content.gsub!(expr, "<span class='highlight-#{a}'>#{word}</span>")
-   end
-   content
+  def wrap_word_in_span(a, b, content)
+    content = content
+
+    b.each do |word|
+      expr = /\b#{word}\b/
+      content.gsub!(expr, "<span class='highlight-#{a}'>#{word}</span>")
+    end
+    
+    content
   end
 end
